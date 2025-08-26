759a4787270a153e7b6ac6915f39e39664da17fe
camipozas <camila.pozas@banca.me>
2025-08-25 20:04:44 -0400

    refactor: Remove redundant migration files for renaming amountCents to amount

--- backoffice/prisma/migrations/20250825193200_rename_amount_cents_to_amount/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Payment" RENAME COLUMN "amountCents" TO "amount";
-
--- Update the values from cents to regular amount (divide by 100)
-UPDATE "Payment" SET "amount" = "amount" / 100;
--- prisma/migrations/20250825193500_rename_amount_cents_to_amount/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Payment" RENAME COLUMN "amountCents" TO "amount";
-
--- Update the values from cents to regular amount (divide by 100)
-UPDATE "Payment" SET "amount" = CASE 
-  WHEN "amount" > 100 THEN "amount" / 100
-  ELSE "amount"
-END;
